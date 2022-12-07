9288483d5f3cda5d19f2307649f4c7b9a5fae6c2
mfrueda <113958218+mfrueda@users.noreply.github.com>
2022-12-06 21:21:52 -0500

    Add files via upload

---
+++ src/components/books/book-edit.jsx
@@ -0,0 +1,34 @@
+import BookForm from "./books-form"
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import dateFormat from "dateformat"
+
+const BookEdit = () => {
+    const { id } = useParams()
+    const [book, setBook] = useState(null)
+
+    useEffect(() => {
+        axios
+            .get(`https://backendmintic-production.up.railway.app/books/${id}`)
+            .then(response => {
+                console.log(response.data)
+                const readAt = response.data.readAt
+                response.data['readAt'] = dateFormat(readAt, "yyyy-mm-dd")
+                setBook(response.data)
+            })
+    }, [])
+
+    return (
+        <>
+            {book ? (
+                <>
+                    <h1>This is the edition form from book # {id}</h1>
+                    <BookForm data={book} bookId={id} />
+                </>
+            ) : ''}
+        </>
+    )
+}
+
+export default BookEdit
---
+++ src/components/books/books-form.jsx
@@ -0,0 +1,87 @@
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+
+
+
+const BookForm = ({ data, bookId }) => {
+    const [isEdit, setIsEdit] = useState(false)
+
+    const { register, formState: { errors }, handleSubmit, setValue } = useForm();
+
+    const customSubmit = (dataForm) => {
+        const bookObject = {
+            title: dataForm.bookName,
+            author: dataForm.authorName,
+            year: dataForm.year,
+            readAt: dataForm.readAt,
+            userId: "63857941aa0cb09677c30304"
+        }
+
+        if (isEdit) { 
+            axios
+                .put(`https://backendmintic-production.up.railway.app/books/edit/${bookId}`, bookObject)
+                .then(response => console.log(response.data))
+        } else {
+            axios
+                .post(`https://backendmintic-production.up.railway.app/books/create`, bookObject)
+                .then(response => console.log(response.data))
+        }
+    }
+
+    useEffect(() => {
+        console.log(data)
+        if (data.length !== 0) {
+            setIsEdit(true)
+            setValue('bookName', data.title)
+            setValue('authorName', data.author)
+            setValue('year', data.year)
+            setValue('readAt', data.readAt)
+        }
+    }, [])
+
+    return (
+        <>
+            <form className="form" onSubmit={handleSubmit(customSubmit)}>
+                <div className="form__item">
+                    <label>Book's Name</label>
+                    <input
+                        {...register("bookName", { required: true })}
+                        aria-invalid={errors.bookName ? "true" : "false"}
+                    />
+                    {errors.bookName && <p>Field required</p>}
+                </div>
+                <div className="form__item">
+                    <label>Author's Name</label>
+                    <input
+                        {...register("authorName", { required: true })}
+                        aria-invalid={errors.authorName ? "true" : "false"}
+                    />
+                    {errors.authorName && <p>Field required</p>}
+                </div>
+                <div className="form__item">
+                    <label>Year</label>
+                    <input
+                        type="number"
+                        {...register("year", { required: true, min: 868, max: 2022 })}
+                        aria-invalid={errors.year ? "true" : "false"}
+                    />
+                    {errors.year && <p>Min 868. Max 2022</p>}
+                </div>
+                <div className="form__item">
+                    <label>Read at</label>
+                    <input
+                        type="date"
+                        {...register("readAt", { required: true })}
+                        aria-invalid={errors.readAt ? "true" : "false"}
+                    />
+                    {errors.readAt && <p>Field required</p>}
+                </div>
+                <input type="submit" value={isEdit ? "Edit" : "Create"} />
+            </form>
+        </>
+    )
+}
+
+export default BookForm
